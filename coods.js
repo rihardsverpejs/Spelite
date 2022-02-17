24ef6e03cacf54c4eec97d85a71d69c74638bb0a
rihardsverpejs <90392253+rihardsverpejs@users.noreply.github.com>
2022-02-17 14:46:54 +0200

    Update coods.js

--- coods.js
+++ coods.js
@@ -1,1 +1,12 @@
-var ctx = myCanvas.getContext("2d");
+var ctx = lauks.getContext("2d");
+var bug_x = 0;
+var bug_y = 0;
+var BugImg = new Image();
+BugImg.src = "https://www.pngkey.com/png/full/32-324021_santa-hat-mario-toad-pixel-art-mario-mushroom.png";
+
+function paradies() {
+   ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+   ctx.drawImage(BugImg, bug_x, bug_y);
+   }
+
+setInterval(paradies, 25);
