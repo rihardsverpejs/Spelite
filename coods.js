b95fe93bb151447882fd0943fa3fc3bd8ce9808f
rihardsverpejs <90392253+rihardsverpejs@users.noreply.github.com>
2022-02-17 15:12:02 +0200

    fy

--- coods.js
+++ coods.js
@@ -1,12 +1,20 @@
-var ctx = lauks.getContext("2d");
+var ctx = document.getElementById("lauks").getContext("2d");
+
 var bug_x = 0;
 var bug_y = 0;
 var BugImg = new Image();
-BugImg.src = "https://www.pngkey.com/png/full/32-324021_santa-hat-mario-toad-pixel-art-mario-mushroom.png";
+BugImg.src = "https://s2js.com/img/etc/ladybug.png";
 
-function paradies() {
-   ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+function paradies () {
+   ctx.clearRect(0, 0, lauks.width, lauks.height);
    ctx.drawImage(BugImg, bug_x, bug_y);
    }
 
 setInterval(paradies, 25);
+function iliketomoveit (wow) {
+    if (wow.keyCode == 39) {bug_x = bug_x + 20;} 
+    if (wow.keyCode == 40) {bug_y = bug_y + 20;} 
+    if (wow.keyCode == 37) {bug_x = bug_x - 20;}
+    if (wow.keyCode == 38) {bug_y = bug_y - 20;}
+    }
+    addEventListener("keydown", iliketomoveit);
